2a9fb938fde147adb4b1c15ba998f0ac027f0250
eric <ana31005@gmail.com>
2023-03-23 03:16:29 +0800

    更新資料

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/posts');
 
 var app = express();
 
